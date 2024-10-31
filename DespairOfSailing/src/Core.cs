fc0fb0acc6e035e04f55ee592f5a59f78345e128
Dana <beware.14b@gmail.com>
2024-11-01 01:28:07 +0200

    Add project files.

---
+++ DespairOfSailing/src/Core.cs
@@ -0,0 +1,11 @@
+﻿using Vintagestory.API.Common;
+
+namespace DespairOfSailing;
+
+public class Core : ModSystem
+{
+    public override void Start(ICoreAPI api)
+    {
+        api.Logger.Event("started '{0}' mod", Mod.Info.Name);
+    }
+}
